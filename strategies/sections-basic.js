3e7b59aad5436f4b06b8ff1b6da98d6a4f201119
lshifr <lshifr@gmail.com>
2018-01-15 04:53:36 +0300

    Removed debug console logs. Made section level group attempt a global variable. Added the getCurrentAttempt() method

--- strategies/sections-basic.js
+++ strategies/sections-basic.js
@@ -9,11 +9,11 @@ class MultiAttemptTestWrapper extends BaseTestWrapper{
     }    
 }
 
-
 class SimpleStrategyWithSections extends BaseTestingStrategy {
     constructor(tests, tester, config) {
         super(tests, tester, config);
         this.currentSection = null;
+        this.currentAttempt = 1;
         this.sections = ['DataTypes', 'ExecutionContextAndObjectMethods'];
         this.testsInSection = 4;
         this.sectionTests = this._prepareTests(tests, this.sections);
@@ -23,13 +23,7 @@ class SimpleStrategyWithSections extends BaseTestingStrategy {
         this.levels = ["Easy", "Moderate", "Hard"]; // Override
         this.testgen = (this._createFullGenerator())();
         this.maxTests = Infinity; // Hard-coded for now
-        /*
-        this.sectionPassCondition = function(sectionTests){
-            let totalQuestions = sectionTests.length;
-            let passedQuestions = sectionTests.filter(test => test.answeredCorrectly).length;
-            return passedQuestions / totalQuestions >= 0.75;
-        }
-        */
+        this.results = null;
     }
 
     getTestWrapper(){
@@ -51,10 +45,6 @@ class SimpleStrategyWithSections extends BaseTestingStrategy {
     }
 
     _createSectionLevelGenerator(section, level){
-        let attempts = 0;
-        console.log('Creating section level generator for section' + section + 'and level' + level);
-        console.log('All tests we have at this point available are:');
-        console.log(this.sectionTests);
         let tests = this.sectionTests[section][level];
         let max_attempts = this.sectionLevelAttempts;
         let passLevel = this.sectionPassLevel;
@@ -63,37 +53,24 @@ class SimpleStrategyWithSections extends BaseTestingStrategy {
             let previous = null;
             let total = 0;
             let passed = 0;
-            while(attempts < max_attempts){
-                console.log('About to begin the next test iteration. Tests:');
-                console.log(tests);
+            while(self.currentAttempt <= max_attempts){
                 for(let test of tests){
                     previous = yield {
                         test: test, 
-                        attempt: attempts
+                        attempt: self.currentAttempt
                     };
                     total += 1;
                     passed += previous.answeredCorrectly? 1 : 0;
                 }
                 if(passed / total >= passLevel){
-                    console.log('Section '+ section + ', level '+ level + ': passed ' + passed + 'of' + total);
                     return true; // Signal that this level has been passed
                 }
-                attempts++;
+                self.currentAttempt++;
                 // Clear previous results
-
-                console.log('Section '+ section + ', level '+ level + ': failed, passing ' + passed + ' out of ' + total);
-                console.log('Starting over, attempt #' + attempts + '\n');
-
                 passed = 0;
                 total = 0;
-
-                tests.forEach(test => {
-                    test.answeredCorrectly = null;
-                    test.userAnswer = null;
-                });
-
-                // self.tester._removeHistory(tests);
             }
+            self.currentAttempt = 1;
             return null;
         }
     }
@@ -114,15 +91,6 @@ class SimpleStrategyWithSections extends BaseTestingStrategy {
 
         let getNextLevel = (up) => {
             return this.levels[this.levels.indexOf(this.currentLevel) + (up ? 1: -1)];
-            /*
-            if(prevResult){
-                return this.levels[this.levels.indexOf(this.currentLevel) + 1];    
-            }
-            if(getMaxPassedLevel()){
-                return null; // It is enough to have just some level passed
-            }
-            return this.levels[this.levels.indexOf(this.currentLevel) -1 ];
-            */
         }
 
         let createLevelGen = () => {
@@ -134,9 +102,6 @@ class SimpleStrategyWithSections extends BaseTestingStrategy {
             let nextLevel = getNextLevel(currentLevelResult);
             if(nextLevel){
                 this.currentLevel = nextLevel;
-                console.log('Moving to the next level: ' + this.currentLevel);
-                console.log('Results so far:');
-                console.log(triedLevels);
             }
             if(!currentLevelResult && getMaxPassedLevel()){
                 return null; 
@@ -152,12 +117,10 @@ class SimpleStrategyWithSections extends BaseTestingStrategy {
                 res = levelGen.next(nxt);
             }
             if (moveToNextLevel(res.value)){
-                res = yield* gen();
-                return res;
+                res = yield* gen();  // NOTE: it is very important to return the result explicitly from parent generator
+                return res;          // The reason is that return from inner generator does not automatically return from the outer one
             } else {
-                let maxlev = getMaxPassedLevel();
-                console.log('Section '+ section + ': max passed level: ' + maxlev);
-                return maxlev;
+                return getMaxPassedLevel();
             }
         }    
     }
@@ -174,20 +137,13 @@ class SimpleStrategyWithSections extends BaseTestingStrategy {
                 while(!res.done){
                     let nxt = yield res.value;
                     res = gen.next(nxt);
-                    console.log('Section ' + currentSection + ' tests. Next:');
-                    console.log(res);
-                    console.log('---------------');
                 }
-                console.log('Section ' + currentSection + ' test is over. Result:');
-                console.log(res.value);
                 triedSections[currentSection] = res.value;
                 currentSection = res.value ? sections.shift() : null;
             }
-            console.log('Test is over. Results:');
-            console.log(triedSections);
+            self.results = triedSections;
         }
     }
-
 
     doSupportSections(){
         return true;
@@ -195,6 +151,10 @@ class SimpleStrategyWithSections extends BaseTestingStrategy {
 
     getCurrentSection(){
         return this.currentSection;
+    }
+
+    getCurrentAttempt(){
+        return this.currentAttempt;
     }
 
     fetchNext(test) {
